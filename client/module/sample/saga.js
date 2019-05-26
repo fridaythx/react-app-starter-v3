ea0d87fa74428411755676dfe92a34fd227bcd15
Friday <573039013@qq.com>
2019-05-26 13:20:47 +0800

    Update saga.js

--- client/module/sample/saga.js
+++ client/module/sample/saga.js
@@ -1,5 +1,5 @@
 /* eslint-disable no-param-reassign */
-import { takeLatest, all, select, call, put } from 'redux-saga/effects';
+import { takeLatest, all, select, call, put, fork } from 'redux-saga/effects';
 
 import * as action from './action';
 import * as service from './service';
@@ -8,6 +8,6 @@ function* submit() {
   yield 1;
 }
 
-export default function*() {
+export default fork(function* saga() {
   yield all([takeLatest(action.SUBMIT.REQUEST, submit)]);
-}
+});
